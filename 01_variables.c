62ca606509e1cbcc82654052ed9bc67720e1be13
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-11-02 13:50:29 +0530

    C Language

---
+++ 01_variables.c
@@ -0,0 +1,26 @@
+// # include <stdio.h>
+// int main()
+// {
+    // int a; // variable declaration
+    // a=6; // variable initialization
+    // printf("The output of this program is %d",a); // printing the variable
+    // return 0;
+// }
+
+// #include <stdio.h>
+// float main()
+// {
+    // float b;
+    // b = 2.5;
+    // printf("The output of this program is %f", b);
+    // return 0;
+// }
+
+// # include <stdio.h>
+// int main()
+// {
+    // char c;
+    // c = 'S';
+    // printf ("%c",c);
+    // return 0;
+// }
