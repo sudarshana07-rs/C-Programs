f24fa126474051a6de69885566b3f40432b34550
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-11-04 18:50:36 +0530

    C Language

---
+++ 02_operators.c
@@ -0,0 +1,21 @@
+/*
+Knowing about Arithemetic operators
+Addition (+), Subtraction (-), Multiplication (*), Division (/), Modulus (%)
+*/
+
+#include <stdio.h>
+int main()
+{
+    int a=5, b=3;
+    int add=a+b;
+    int sub=a-b;
+    int mul=a*b;
+    int div=a/b;
+    int mod=a%b;
+    printf("Addition=%d \n",add);
+    printf("Subtraction=%d \n",sub);
+    printf("Multiplication=%d \n",mul);
+    printf("Division=%d \n",div);
+    printf("Modulus=%d \n",mod);
+    return 0;
+}
