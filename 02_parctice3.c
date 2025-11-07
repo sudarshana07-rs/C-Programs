6f48cb89b155da162a8d1596e90c6674ade73906
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-11-07 18:26:39 +0530

    C Language

---
+++ 02_parctice3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Enter the year to check the leap year; ");
+    scanf("%d", &year);
+    if(year%400==0 || year%4==0 && year%100!=0)
+    {
+        printf("%d is a leap year !!! \n", year);
+    }
+    else 
+    {
+        printf("%d is not leap year",year);
+    }
+}
