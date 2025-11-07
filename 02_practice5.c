b6bfb80593033d51c1ba137a1641cad3d934613e
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-11-07 20:31:57 +0530

    C Language

---
+++ 02_practice5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2,n3,n4;
+    printf("Enter 4 numbers:");
+    scanf("%d %d %d %d", &n1,&n2,&n3,&n4);
+    if(n1>n2 && n1>n3 && n1>n4)
+    {
+        printf("%d is greatest of all numbers", n1);
+    }
+    else if(n2>n1 && n2>n3 && n2>n4)
+    {
+        printf("%d is greatest of all numbers", n2);
+    }
+    else if(n3>n2 && n3>n1 && n3>n4)
+    {
+        printf("%d is greatest of all numbers", n3);
+    }
+    else if(n4>n2 && n4>n3 && n4>n1)
+    {
+        printf("%d is greatest of all numbers", n4);
+    }
+    else
+    {
+        printf("Invalid input !!!");
+    }
+}
