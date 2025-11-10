0b3fbe1967ef314cc4505bdd32ea90a380b4eb83
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-11-10 19:29:11 +0530

    C Language

--- 04_loops.c
+++ 04_loops.c
@@ -4,8 +4,11 @@ int main()
     int i=0;
     while (i<=20)
     {
-        printf("The value of i is %d \n",i);
-        i=i+1;
+        if(i>=10)
+        {
+            printf("The value of i is %d \n",i);
+        }
+    i=i+1;
     }
     return 0;
 }
