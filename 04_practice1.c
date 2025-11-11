4bb6d37b3ae13b0d431442a464239bdc15ae4b95
sudarshana07-rs <sudarshana.rs07@gmail.com>
2025-11-11 19:15:39 +0530

    C Language

--- 04_practice1.c
+++ 04_practice1.c
@@ -4,11 +4,11 @@ int main()
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-    int i=0;
-    while (i<=10)
+    int i = 0;
+    while (i <= 10)
     {
-        int mul=n*i;
-        printf("%d * %d = %d \n", n ,i,mul );
+        int mul = n * i;
+        printf("%d * %d = %d \n", n, i, mul);
         i++;
     }
     return 0;
